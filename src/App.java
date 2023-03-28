1e9912f5144f466f8cc4291fdb60ed054b8e25b6
VeronicaCMuller <119083284+VeronicaCMuller@users.noreply.github.com>
2023-03-28 11:38:40 -0300

    ajuste } linha 35-36

--- src/App.java
+++ src/App.java
@@ -33,7 +33,7 @@ public class App {
                 System.out.println("⭐");
             }else if (nota > 3.0 && nota <= 5.0) {
                 System.out.println("⭐⭐");
-            else if (nota > 5.0 && nota <= 7.0){
+            }else if (nota > 5.0 && nota <= 7.0){
                 System.out.println("⭐⭐⭐");
             }else if (nota > 7.0 && nota <= 9.0){
                 System.out.println("⭐⭐⭐⭐");
