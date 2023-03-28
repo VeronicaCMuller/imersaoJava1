021b677b3216a85a5c3dee94993b8896aade0c28
VeronicaCMuller <119083284+VeronicaCMuller@users.noreply.github.com>
2023-03-28 11:26:36 -0300

    ajuste notas

--- src/App.java
+++ src/App.java
@@ -31,13 +31,13 @@ public class App {
             double nota = Double.parseDouble(serie.get("imDbRating"));
             if (nota <= 3.0) {
                 System.out.println("⭐");
-            }else if (nota > 3.0 && nota < 5.1) {
+            }else if (nota > 3.0 && nota <= 5.0) {
                 System.out.println("⭐⭐");
-            }else if (nota > 5.0 && nota > 7.1){
+            else if (nota > 5.0 && nota <= 7.0){
                 System.out.println("⭐⭐⭐");
-            }else if (nota > 7.0 && nota < 8.9){
+            }else if (nota > 7.0 && nota <= 9.0){
                 System.out.println("⭐⭐⭐⭐");
-            }else if (nota >= 9.0 && nota < 10.0) {
+            }else if (nota > 9.0 && nota <= 10.0) {
                 System.out.println("⭐⭐⭐⭐⭐");
             }
             System.out.println("\n");
