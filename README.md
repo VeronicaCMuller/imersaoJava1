aa135c508a39c73d66b153f86b8694e872d6f154
VeronicaCMuller <veronicacamposmuller@gmail.com>
2023-03-28 10:56:59 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+<h1 align="center"> Imersão JAVA da Alura </h1>
+
+<p align="center">
+Imersão JAVA é um evento exclusivo e gratuito, promovido pela Alura para ensino da linguagem JAVA. <br/>
+</p>
+
+<br>
+
+## ✨ Minha experiência
+
+Primeira aula de uma semana de imersão junto com a Alura, onde tive o prazer de aprender na prática atividades Back-end com a linguagem JAVA. Experimentamos ações e ferrmentas úteis na vida de um dev, além de ter acesso a uma comunidade incrível com alunos em diversos níveis de conhecimento.
+  
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- JAVA
+- Http
+- Json
+- Git e Github
+- Visual Studio Code
+
+## 💻 Projeto
+    
+   <h3> Aula 1 - Consumindo uma API de Filmes com Java. </h3>
+   <p>
+Iniciamos realizando conexão Http para buscar top 250 filmes do site IMDB, extraimos somente os dados de interesse, manipulamos e exibimos.
+
+    • Desafios do primeiro dia:
+
+        ⒈ Consumir o endpoint de filmes mais populares da API do IMDB. Procure também, na documentação da API do IMDB, o endpoint que retorna as melhores séries e o que retorna as séries mais populares.
+        
+        ⒉ Usar sua criatividade para deixar a saída dos dados mais bonitinha: usar emojis com código UTF-8, mostrar a nota do filme como estrelinhas, decorar o terminal com cores, negrito e itálico usando códigos ANSI, e mais!
+        
+        ⒊ Colocar a chave da API do IMDB em algum lugar fora do código como um arquivo de configuração (p. ex, um arquivo .properties) ou uma variável de ambiente.
+</p>
+
+## 🙌 Agradecimentos
+
+    
+  Quero agradecer à equipe Alura, em especial aos professores Paulo Silveira, Jacqueline Oliveira e Alexandre Aquiles, por essa experiência incrivel.
---
+++ src/App.java
@@ -0,0 +1,46 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // Fazer um aconexão HTTP e buscar os top 250 filmes
+        String url =  "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularTVs.json";
+        URI address = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request =  HttpRequest.newBuilder(address).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+           
+        // Extrair somente os dados de interesse (extração do título, poster, classificação) - parciar.
+        JsonParser parser = new JsonParser();
+        List<Map<String, String>>listaDeSeries = parser.parse(body);
+        //Utilitário Lista< Utilitario Map que associa uma chave com o valor <Tipo da chave, tipo do valor associado à essa chave> dados que queremos = 
+       
+        // Exibir e manipular os dados.
+        for (int i = 0; i < 3; i++) {
+            Map<String,String> serie = listaDeSeries.get(i);
+            System.out.println("\u001b[1m Título:\u001b[m" + serie.get("title"));
+            System.out.println("\u001b[1m Poster:\u001b[m" + serie.get("image"));
+            System.out.println("\u001b[37;1m \u001b[45;1mClassificação:\u001b[m " + serie.get("imDbRating"));
+            double nota = Double.parseDouble(serie.get("imDbRating"));
+            if (nota < 3.0) {
+                System.out.println("⭐");
+            }else if (nota > 3.0 && nota < 5.1) {
+                System.out.println("⭐⭐");
+            }else if (nota > 5.0 && nota > 7.1){
+                System.out.println("⭐⭐⭐");
+            }else if (nota > 7.0 && nota < 8.9){
+                System.out.println("⭐⭐⭐⭐");
+            }else if (nota >= 9.0 && nota < 10.0) {
+                System.out.println("⭐⭐⭐⭐⭐");
+            }
+            System.out.println("\n");
+        }
+    }
+}
